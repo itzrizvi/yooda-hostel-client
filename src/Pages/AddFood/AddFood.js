965b206a89e067b696a1123011956d0e8806f6e8
itzrizvi <shahriar.me.work@gmail.com>
2022-02-09 03:21:21 +0600

    ADD EDIT AND DELETE FOOD DONE

--- src/Pages/AddFood/AddFood.js
+++ src/Pages/AddFood/AddFood.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
+import FoodRow from '../FoodRow/FoodRow';
 import Navigation from '../Shared/Navigation/Navigation';
 
 const AddFood = () => {
-    // State for getting food data
+    // State for posting food data
     const [food, setFood] = useState({
         foodItem: "",
         costPrice: "",
     });
+
+    // State for getting all foods
+    const [allFoods, setAllFoods] = useState([]);
 
     // Form Change Function
     const handleChange = ({ currentTarget: input }) => {
@@ -17,8 +22,28 @@ const AddFood = () => {
     // Form Submit Function
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(food);
+        fetch(`http://localhost:5000/FoodItem/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(food)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('A New Food Item Has Been Added Successfully!!');
+                    // window.location.reload();
+                }
+            });
     }
+
+    // FETCH FOOD ITEM FOR TABLE
+    useEffect(() => {
+        fetch(`http://localhost:5000/FoodItem/`)
+            .then(res => res.json())
+            .then(data => setAllFoods(data));
+    }, [food, allFoods]);
 
     return (
         <>
@@ -31,9 +56,9 @@ const AddFood = () => {
                     <div className="col-md-12">
                         <form className='addfood-form' onSubmit={handleSubmit}>
                             <label htmlFor="addfoodin">Food Name</label>
-                            <input type="text" name="foodItem" value={food.foodItem} onChange={handleChange} id='addfoodin' placeholder='Food name...' required />
+                            <input type="text" name="foodItem" onChange={handleChange} id='addfoodin' placeholder='Food name...' required />
                             <label htmlFor="addfoodprice">Food Price</label>
-                            <input type="text" name="costPrice" value={food.costPrice} onChange={handleChange} id='addfoodprice' placeholder='Food price...' required />
+                            <input type="text" name="costPrice" onChange={handleChange} id='addfoodprice' placeholder='Food price...' required />
                             <input type="submit" className='addfood-btn' value="Add Food" />
                         </form>
                     </div>
@@ -42,7 +67,23 @@ const AddFood = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        <h2>Food Table</h2>
+                        <h2 className='food-table-title'>Available Foods</h2>
+                    </div>
+                    <div className="col-md-12">
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Food Item</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {allFoods.map(singleFood => <FoodRow
+                                    key={singleFood._id}
+                                    singleFood={singleFood}></FoodRow>)}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
---
+++ src/Pages/FoodRow/FoodRow.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+
+const FoodRow = ({ singleFood }) => {
+    // state For update input
+    const [isEdit, setIsEdit] = useState(false);
+    // Set Edited Food Name
+    const [editFoodName, setEditFoodName] = useState('');
+    // Set Edited Food Price
+    const [editFoodPrice, setEditFoodPrice] = useState('');
+
+
+    // Edit Food Name Submit
+    const editFoodSubmit = (e) => {
+        e.preventDefault();
+        const updatedFoodName = {
+            editFoodName,
+            editFoodPrice
+        }
+        const url = `http://localhost:5000/FoodItem/${singleFood._id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedFoodName)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert('Updated Food Data');
+                    setIsEdit(false)
+                }
+            })
+    }
+
+    // Delete Order Function
+    const handleDelete = id => {
+        const proceed = window.confirm('Are You sure you want to delete this Food Item??');
+        if (proceed) {
+            fetch(`http://localhost:5000/FoodItem/${id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('A Food Item Deleted Successfully!!');
+                        // Reloading window for not updating restOrders after calling it though
+                        // window.location.reload();
+                    }
+                });
+        }
+    }
+
+    return (
+        <tr>
+            {isEdit && <td>
+                <form className='foodName-edit' onSubmit={editFoodSubmit}>
+                    <input type="text" onChange={(e) => setEditFoodName(e.target.value)} placeholder='Update Food Name...' required />
+                    <input type="text" onChange={(e) => setEditFoodPrice(e.target.value)} placeholder='Update Food Price...' required />
+                    <input type="submit" value="Update" />
+                </form>
+            </td>}
+            <td style={{ fontWeight: '700' }}>{singleFood.foodItem} <button onClick={() => setIsEdit(true)} className='fooditem-edit'>Edit</button></td>
+            <td style={{ fontWeight: '700' }}>{singleFood.costPrice}TK  <button className='fooditem-edit' onClick={() => setIsEdit(true)}>Edit</button></td>
+            <td><button className='fooditem-delete' onClick={() => handleDelete(singleFood._id)}>Delete</button></td>
+        </tr>
+    );
+};
+
+export default FoodRow;
