7241bd13f8ef56529147eb58cb04e1dc62ab11c8
itzrizvi <shahriar.me.work@gmail.com>
2022-02-08 22:03:11 +0600

    Add FOOD Working

--- src/Pages/AddFood/AddFood.js
+++ src/Pages/AddFood/AddFood.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import Navigation from '../Shared/Navigation/Navigation';
 
 const AddFood = () => {
+    // State for getting food data
+    const [food, setFood] = useState({
+        foodItem: "",
+        costPrice: "",
+    });
+
+    // Form Change Function
+    const handleChange = ({ currentTarget: input }) => {
+        setFood({ ...food, [input.name]: input.value });
+    };
+
+    // Form Submit Function
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(food);
+    }
+
     return (
         <>
             <Navigation />
@@ -9,10 +27,23 @@ const AddFood = () => {
                 <h3>Add Food Here</h3>
             </div>
             <div className="container">
-                <div className="col-md-12">
-                    <form className='addfood-form'>
-
-                    </form>
+                <div className="row">
+                    <div className="col-md-12">
+                        <form className='addfood-form' onSubmit={handleSubmit}>
+                            <label htmlFor="addfoodin">Food Name</label>
+                            <input type="text" name="foodItem" value={food.foodItem} onChange={handleChange} id='addfoodin' placeholder='Food name...' required />
+                            <label htmlFor="addfoodprice">Food Price</label>
+                            <input type="text" name="costPrice" value={food.costPrice} onChange={handleChange} id='addfoodprice' placeholder='Food price...' required />
+                            <input type="submit" className='addfood-btn' value="Add Food" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2>Food Table</h2>
+                    </div>
                 </div>
             </div>
         </>
