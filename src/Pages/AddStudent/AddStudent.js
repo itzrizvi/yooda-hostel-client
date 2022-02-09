9957e83d1489765a16d38682bd473bb23e94ec22
itzrizvi <shahriar.me.work@gmail.com>
2022-02-10 01:56:19 +0600

    Student Update Done

--- src/Pages/AddStudent/AddStudent.js
+++ src/Pages/AddStudent/AddStudent.js
@@ -1,7 +1,57 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import Navigation from '../Shared/Navigation/Navigation';
 
 const AddStudent = () => {
+
+    // Selected Student ID for Update
+    const [selectedStudent, setSelectedStudent] = useState("");
+    // Updating Student 
+    const [updatedStudent, setUpdatedStudent] = useState({
+        fullName: "",
+        roll: "",
+        class: "",
+        age: "",
+        hallName: "",
+        status: ""
+    })
+
+    // Bootstrap Modal Open Close State
+    const [show, setShow] = useState(false);
+    // Bootstrap Modal Open Close Function
+    const handleClose = () => setShow(false);
+    const handleShow = (student) => {
+        setSelectedStudent(student);
+        setShow(true)
+    };
+
+    // Getting Updated Data
+    const studentUpdateChange = ({ currentTarget: input }) => {
+        setUpdatedStudent({ ...updatedStudent, [input.name]: input.value });
+    }
+
+    // Submit Updated Data Function
+    const handleUpdateSubmit = (e) => {
+        e.preventDefault();
+        console.log(updatedStudent);
+
+        const url = `http://localhost:5000/Student/${selectedStudent._id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedStudent)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert('Updated Student Data');
+                    window.location.reload();
+                }
+            })
+    }
+
     // Student ID State
     const [studentId, setStudentId] = useState([]);
 
@@ -131,6 +181,44 @@ const AddStudent = () => {
                         <h2 className='student-table-title'>All Students</h2>
                     </div>
                     <div className="col-md-12">
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Update Data of {selectedStudent.fullName} </Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <form className='studentDataUpdate-form' onSubmit={handleUpdateSubmit}>
+                                    <label htmlFor="upstdnfullname">Full Name</label>
+                                    <input type="text" name="fullName" onChange={studentUpdateChange} id='upstdnfullname' placeholder='Full name...' required />
+
+                                    <label htmlFor="upstndroll">Roll</label>
+                                    <input type="text" name="roll" id='upstndroll' onChange={studentUpdateChange} placeholder='Student Roll...' required />
+
+                                    <label htmlFor="upstndclass">Class</label>
+                                    <input type="text" name="class" id='upstndclass' onChange={studentUpdateChange} placeholder='Student Class...' required />
+
+                                    <label htmlFor="upstndage">Age</label>
+                                    <input type="text" name="age" id='upstndage' onChange={studentUpdateChange} placeholder='Student Age...' required />
+
+                                    <label htmlFor="upstndhall">Hall Name</label>
+                                    <input type="text" name="hallName" id='upstndhall' onChange={studentUpdateChange} placeholder='Hall Name...' required />
+
+                                    <p className='stdn-stts'>Status:</p>
+                                    <div className="status d-flex">
+                                        <input type="radio" id="upactive" name="status" onChange={studentUpdateChange} value="Active" />
+                                        <label htmlFor="upactive">Active</label>
+                                        <input type="radio" id="upinactive" name="status" onChange={studentUpdateChange} value="Inactive" />
+                                        <label htmlFor="upinactive">Inactive</label>
+                                    </div>
+
+                                    <input type="submit" className='addstudent-btn' value="Update Student" />
+                                </form>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Close
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
                         <table className="table table-hover">
                             <thead>
                                 <tr>
@@ -140,6 +228,7 @@ const AddStudent = () => {
                                     <th scope="col">Age</th>
                                     <th scope="col">Hall Name</th>
                                     <th scope="col">Status</th>
+                                    <th scope="col">Action</th>
                                     <th scope="col">
                                         <form className='statusChangeButton-form' onSubmit={handleStatusSubmit}>
                                             <button type='submit' onClick={() => setIsStatus("Active")} className='statusChange-btn'>Set Active</button>
@@ -149,6 +238,7 @@ const AddStudent = () => {
                                     </th>
                                 </tr>
                             </thead>
+
                             <tbody>
                                 {allStudents.map(singleStudent => <tr key={singleStudent._id}>
                                     <td style={{ fontWeight: '700' }}>{singleStudent.roll}</td>
@@ -158,12 +248,21 @@ const AddStudent = () => {
                                     <td style={{ fontWeight: '700' }}>{singleStudent.hallName}</td>
                                     <td style={{ fontWeight: '700' }}>{singleStudent.status}</td>
                                     <td style={{ fontWeight: '700' }}>
+                                        <Button className='studentdatadelete-btn' onClick={() => handleShow(singleStudent)}>
+                                            Edit
+                                        </Button>
+                                        <Button className='studentdatadelete-btn'>
+                                            Delete
+                                        </Button>
+                                    </td>
+                                    <td style={{ fontWeight: '700' }}>
                                         <input
                                             onClick={() => handleStudentId(singleStudent?._id)}
                                             type="checkbox"
                                             name="status"
                                             id="stsChangeBulk" />
                                     </td>
+
                                 </tr>)}
                             </tbody>
                         </table>
